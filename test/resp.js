ad49d5f767696b52e60bac9faec16189b36dccae
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2014-01-23 22:03:24 -0500

    Initial working version.
    Includes tests.

---
+++ test/resp.js
@@ -0,0 +1,68 @@
+var expect = require('chai').expect
+  , resp = require('../lib')
+
+describe('Resp', function () {
+  describe('stringify', function () {
+    it('should stringify Status objects', function () {
+      expect(resp.stringify({
+        message: 'Some message'
+      })).to.equal('+Some message\r\n')
+    })
+
+    it('should stringify Error objects', function () {
+      expect(resp.stringify({
+        name: 'BADERR',
+        message: 'Some message'
+      })).to.equal('-BADERR Some message\r\n')
+    })
+
+    it('should stringify booleans', function () {
+      expect(resp.stringify(true)).to.equal(':1\r\n')
+    })
+
+    it('should stringify numbers', function () {
+      expect(resp.stringify(123)).to.equal(':123\r\n')
+    })
+
+    it('should stringify strings', function () {
+      expect(resp.stringify('foobar')).to.equal('$6\r\nfoobar\r\n')
+    })
+
+    it('should stringify arrays', function () {
+      expect(resp.stringify([1, 2, 'foo', 'bar', 5, false]))
+        .to.equal('*6\r\n:1\r\n:2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n:5\r\n:0\r\n')
+    })
+  })
+
+  describe('parse', function () {
+    it('should parse Status objects', function () {
+      expect(resp.parse('+Some message\r\n')).to.deep.equal({
+        message: 'Some message'
+      })
+    })
+
+    it('should parse Error objects', function () {
+      expect(resp.parse('-BADERR Some message\r\n')).to.deep.equal({
+        name: 'BADERR',
+        message: 'Some message'
+      })
+    })
+
+    it('should parse "booleans" as a number', function () {
+      expect(resp.parse(':1\r\n')).to.equal(1)
+    })
+
+    it('should parse numbers', function () {
+      expect(resp.parse(':123\r\n')).to.equal(123)
+    })
+
+    it('should parse strings', function () {
+      expect(resp.parse('$6\r\nfoobar\r\n')).to.equal('foobar')
+    })
+
+    it('should parse arrays', function () {
+      expect(resp.parse('*6\r\n:1\r\n:2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n:5\r\n:0\r\n'))
+        .to.deep.equal([1, 2, 'foo', 'bar', 5, 0])
+    })
+  })
+})
